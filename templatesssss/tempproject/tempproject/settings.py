f1a1da34e40d87f4237a6c5e7738862d85ec9e30
satyam43013 <satyambadal07@gmail.com>
2024-05-03 22:01:50 +0530

    ok

--- templatesssss/tempproject/tempproject/settings.py
+++ templatesssss/tempproject/tempproject/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # print(BASE_DIR)
-new_template_path = os.path.join(BASE_DIR,'Templates')
+new_template_path = os.path.join(BASE_DIR,'templates')
 print(new_template_path)
 
 # Quick-start development settings - unsuitable for production
