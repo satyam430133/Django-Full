1769571ea4905c245d81a1ef160fb496464a2ecb
satyam43013 <satyambadal07@gmail.com>
2024-05-04 12:30:02 +0530

    ok

--- StaticLoad/Project/App/urls.py
+++ StaticLoad/Project/App/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home/',Home)
+    path('home/',Home,name='index'),
+    path('about/',About,name='about'),
+    path('contect/',Contect,name='contect'),
 ]
--- StaticLoad/Project/App/views.py
+++ StaticLoad/Project/App/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def Home(request):
     return render(request, 'index.html')
+
+def About(request):
+    return render(request, 'about.html')
+
+def Contect(request):
+    return render(request, 'contect.html')
+
+
+    
