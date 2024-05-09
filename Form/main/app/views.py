7c2fb69f69f6e588783f2865034ed77739cab78b
satyam430133 <satyambadal07@gmail.com>
2024-05-09 12:27:02 +0530

    ok

--- Form/main/app/views.py
+++ Form/main/app/views.py
@@ -1,32 +1,35 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import InfoModel
 from app.forms import StudentForm
-# Create your views here.
 
 def Home(request):
-    context = {}
-    context['form'] = StudentForm
-    return render(request, 'index.html',context)
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            msg = "Data Saved"
+            return render(request, 'index.html', {'key': msg})
+    else:
+        form = StudentForm()
+    return render(request, 'index.html', {'form': form})
 
 def Registration(request):
-   try:
-        if request.method == 'POST':
-            Names = request.POST.get('Name')
-            Emails = request.POST.get('Email')
-            Passwords = request.POST.get('Password')
-            if Names and Emails and Passwords != '':
-                fill = InfoModel.objects.filter(Email = Emails)
-                if fill:
-                    msg = "already registered"
-                    return render(request, 'index.html',{'key':msg})
-                else:
-                    msg = "Data Saved"
-                    okay = InfoModel.objects.create(Name=Names,Email=Emails,Password=Passwords)
-                    return render(request, 'index.html',{'key':msg})
-                msg = "Please Fill Details"
-                return render(request, 'index.html',{'key':msg})
+    if request.method == 'POST':
+        Names = request.POST.get('Name')
+        Emails = request.POST.get('Email')
+        Passwords = request.POST.get('Password')
+        if Names and Emails and Passwords != '':
+            fill = InfoModel.objects.filter(Email=Emails)
+            if fill:
+                msg = "already registered"
+                return redirect('index')
             else:
-                return render(request, 'index.html')
-   except:
-        msg = "Code Error"
-        return render(request, 'index.html',{'key':msg})
+                msg = "Data Saved"
+                okay = InfoModel.objects.create(Name=Names, Email=Emails, Password=Passwords)
+            return render(request, 'index.html', {'key': msg})
+        else:
+            msg = "Please Fill Details"
+            return render(request, 'index.html', {'key': msg})
+        return redirect('index')
+    else:
+        return render(request, 'index.html')
