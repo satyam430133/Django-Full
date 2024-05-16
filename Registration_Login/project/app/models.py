1fa3c11deb76f885e9b3b1efacc2271d2ec48280
satyam430133 <satyambadal07@gmail.com>
2024-05-16 13:26:24 +0530

    ok

--- Registration_Login/project/app/models.py
+++ Registration_Login/project/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    Name = models.CharField(max_length=100)
+    Email = models.EmailField()
+    Phone_Number = models.IntegerField()
+    Password = models.CharField(max_length=100)
+    Confirm_Password = models.CharField(max_length=100)
+    class Meta():
+        verbose_name_plural = 'UserList'
+    def __str__(self):
+        return str(self.Name)
--- Registration_Login/project/app/views.py
+++ Registration_Login/project/app/views.py
@@ -1,13 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
-
+from .models import *
 # Create your views here.
 
 def home(request):
     return render(request,'index.html')
 
 def Registration(request):
-    return render(request,'registration.html')
+    if request.method == 'POST':
+        unames = request.POST.get('username')
+        emails = request.POST.get('email')
+        phones = request.POST.get('phone')
+        passwords = request.POST.get('password')
+        cpass = request.POST.get('confirm_password')
+        userdata = User.objects.filter(Email=emails)
+        if userdata:
+            msg = 'User Already Exist Login'
+            return render(request,'login.html',{'key':msg})
+        else:
+            if passwords == cpass:
+                User.objects.create(Name=unames,Email=emails,Phone_Number=phones,Password=passwords,Confirm_Password=cpass)
+                msg = "Registration Successfull"
+                return render(request,'login.html',{'key':msg})
+            msg = 'Password & Confirm Password Does Not Match'
+            return render(request,'registration.html',{'key':msg})
+        return render(request,'registration.html')
+    else:
+        return render(request,'registration.html')
 
 def Login(request):
+    if request.method == 'POST':
+        loginemail = request.POST.get('login_email')
+        loginpass = request.POST.get('login_password')
+        userdata = User.objects.get(Email=loginemail)
+        if userdata.Email == loginemail:
+            user = User.objects.get(Email=loginemail)
+            alluser = User.objects.all()
+            msg = 'Login Successfull'
+            return render(request,'index.html',{'key':msg,'user':user,'alluser':alluser})
+        else:
+            msg = 'Not A User Register First'
+            return redirect('register',{'key':msg}) 
     return render(request,'login.html')
